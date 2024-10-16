af35a705fd6d5dcc909a089ddc3f99e0bfce2b0d
Joe <jrbevan537@gmail.com>
2024-10-16 18:55:32 +0200

    implement info page

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -34,6 +34,15 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const numPersons = persons.length;
+  const now = new Date();
+
+  response.send(`<p>The phonebook has information for ${numPersons} people.</p>
+  <p>${now}</p>`)
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
